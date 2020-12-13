77a8daea147624c1cc7c287ab13be3b9c1aabbbc
BaptisteVilledieu <baptiste.villedieu2000@gmail.com>
2020-12-13 13:42:35 +0100

    tests pour isFreeFor réalisés

--- src/test/java/agenda/AgendaTest.java
+++ src/test/java/agenda/AgendaTest.java
@@ -21,12 +21,16 @@ public class AgendaTest {
     // November 1st, 2020, 22:30
     LocalDateTime nov_1__2020_22_30 = LocalDateTime.of(2020, 11, 1, 22, 30);
 
+    // November 2nd, 2020, 14:00
+    LocalDateTime nov_2__2020_14_00 = LocalDateTime.of(2020, 11, 2, 14, 00);
+    
     // 120 minutes
     Duration min_120 = Duration.ofMinutes(120);
 
     // A simple event
     // November 1st, 2020, 22:30, 120 minutes
     Event simple = new Event("Simple event", nov_1__2020_22_30, min_120);
+    Event simple2 = new Event("Simple event", nov_2__2020_14_00, min_120);
 
     // A Weekly Repetitive event ending at a given date
     RepetitiveEvent fixedTermination = new FixedTerminationEvent("Fixed termination weekly", nov_1__2020_22_30, min_120, ChronoUnit.WEEKS, jan_5_2021);
@@ -51,6 +55,16 @@ public class AgendaTest {
     public void testMultipleEventsInDay() {
         assertEquals(4, agenda.eventsInDay(nov_1_2020).size(), "Il y a 4 événements ce jour là");
         assertTrue(agenda.eventsInDay(nov_1_2020).contains(neverEnding));
+    }
+    
+    @Test
+    public void testIsFree(){
+        assertFalse(agenda.isFreeFor(simple),"L'évènement ne doit pas être ajouté à l'agenda");
+    }
+    
+    @Test
+    public void testIsFreeV2(){
+        assertTrue(agenda.isFreeFor(simple2), "L'évènement doit être ajouté a l'agenda");
     }
 
 
